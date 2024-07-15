d593e9c36034c75da478735717b2a295f5cde890
moricode49 <samhurley4@hotmail.com>
2024-07-15 12:20:30 +0100

    add ticket-3, get all endpoints completed

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -3,9 +3,21 @@ const request = require("supertest");
 const app = require("../db/app");
 const seed = require("../db/seeds/seed");
 const data = require("../db/data/test-data/index");
+const endpoints = require("../endpoints.json");
 
 beforeEach(() => seed(data));
 afterAll(() => db.end());
+
+describe("/api", () => {
+	test("GET 200, responds with all the avalable endpoints", () => {
+		return request(app)
+			.get("/api")
+			.expect(200)
+			.then(({ body }) => {
+				expect(body.endpoints).toEqual(endpoints);
+			});
+	});
+});
 
 describe("/api/topics", () => {
 	describe("GET", () => {
