b6f2a86518494ac3d7bd5ba660aac629079c041b
moricode49 <samhurley4@hotmail.com>
2024-07-15 11:51:06 +0100

    add task-2, get all topics complete

---
+++ db/controllers/topics.controllers.js
@@ -0,0 +1,9 @@
+const { fetchTopics } = require("../models/topics.models");
+
+function getTopics(request, response, next) {
+	return fetchTopics().then((topics) => {
+		response.status(200).send(topics);
+	});
+}
+
+module.exports = { getTopics };
