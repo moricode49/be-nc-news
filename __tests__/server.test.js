0fb4cd7d0c60c44cfe614e91101beff0ae4e5cc5
moricode49 <samhurley4@hotmail.com>
2024-07-18 20:41:24 +0100

    moved file locations

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -1,6 +1,6 @@
 const db = require("../db/connection");
 const request = require("supertest");
-const app = require("../db/app");
+const app = require("../app");
 const seed = require("../db/seeds/seed");
 const data = require("../db/data/test-data/index");
 const endpoints = require("../endpoints.json");
