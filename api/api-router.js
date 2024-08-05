4b4e87d68e550694fd5d70afe95b9f32834bc759
moricode49 <samhurley4@hotmail.com>
2024-08-05 09:41:32 +0100

    allow cross origin resource sharing

--- api/api-router.js
+++ api/api-router.js
@@ -1,6 +1,27 @@
 const apiRouter = require("express").Router();
+const articlesRouter = require("./articles-router");
 const endpoints = require("../endpoints.json");
+const {
+	getTopics,
+	getArticleById,
+	getArticles,
+	getCommentsByArticleId,
+	postNewComment,
+	updateArticleById,
+	deleteComment,
+	getUsers,
+} = require("../db/controllers/topics.controllers");
 
 apiRouter.get("/", (request, response) => {
-	response.status(200).send("All OK from /api");
+	response.status(200).send({ endpoints });
 });
+
+apiRouter.get("/topics", getTopics);
+
+apiRouter.get("/users", getUsers);
+
+apiRouter.use("/articles", articlesRouter);
+
+apiRouter.delete("/comments/:comment_id", deleteComment);
+
+module.exports = apiRouter;
--- app.js
+++ app.js
@@ -1,41 +1,12 @@
 const express = require("express");
 const app = express();
-const {
-	getTopics,
-	getArticleById,
-	getArticles,
-	getCommentsByArticleId,
-	postNewComment,
-	updateArticleById,
-	deleteComment,
-	getUsers,
-} = require("./db/controllers/topics.controllers");
-const endpoints = require("./endpoints.json");
-// const apiRouter = require("./api/api-router");
+const apiRouter = require("./api/api-router");
+const cors = require("cors");
 
+app.use(cors());
 app.use(express.json());
 
-// app.use("/api", apiRouter);
-
-app.get("/api", (request, response) => {
-	response.status(200).send({ endpoints });
-});
-
-app.get("/api/topics", getTopics);
-
-app.get("/api/articles", getArticles);
-
-app.get("/api/users", getUsers);
-
-app.get("/api/articles/:article_id", getArticleById);
-
-app.get("/api/articles/:article_id/comments", getCommentsByArticleId);
-
-app.post("/api/articles/:article_id/comments", postNewComment);
-
-app.patch("/api/articles/:article_id", updateArticleById);
-
-app.delete("/api/comments/:comment_id", deleteComment);
+app.use("/api", apiRouter);
 
 //400 (SQL) errors
 app.use((error, request, response, next) => {
